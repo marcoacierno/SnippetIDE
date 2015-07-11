dcd8cfb3f64c0242a1e2505e5b1a9bb8c9f66c10
Marco Acierno <marcoaciernoemail@gmail.com>
2015-07-11 19:29:17 +0200

    Handle eventbus DeadEvent and Exception cases

--- Snippet-IDE/src/main/java/com/besaba/revonline/snippetide/events/manager/impl/EventBusEventManager.java
+++ Snippet-IDE/src/main/java/com/besaba/revonline/snippetide/events/manager/impl/EventBusEventManager.java
@@ -2,11 +2,25 @@ package com.besaba.revonline.snippetide.events.manager.impl;
 
 import com.besaba.revonline.snippetide.api.events.Event;
 import com.besaba.revonline.snippetide.api.events.manager.EventManager;
+import com.google.common.eventbus.DeadEvent;
 import com.google.common.eventbus.EventBus;
+import com.google.common.eventbus.Subscribe;
+import org.apache.log4j.Logger;
 import org.jetbrains.annotations.NotNull;
 
 public class EventBusEventManager implements EventManager {
-  private final EventBus eventBus = new EventBus();
+  private final static Logger logger = Logger.getLogger(EventBusEventManager.class);
+
+  private final EventBus eventBus = new EventBus((throwable, subscriberExceptionContext) -> {
+    logger.fatal("Exception inside the EventBus");
+    logger.fatal("Throwable", throwable);
+    logger.fatal("Event: " + subscriberExceptionContext.getEvent());
+    logger.fatal("Method: " + subscriberExceptionContext.getSubscriberMethod());
+  });
+
+  {
+    eventBus.register(this);
+  }
 
   public void registerListener(@NotNull final Object listener) {
     eventBus.register(listener);
@@ -18,5 +32,12 @@ public class EventBusEventManager implements EventManager {
 
   public void post(@NotNull final Event<?> event) {
     eventBus.post(event);
+  }
+
+  @Subscribe
+  public void deadEvent(DeadEvent deadEvent) {
+    logger.fatal("Dead event!");
+    logger.fatal("Event: " + deadEvent.getEvent());
+    logger.fatal("Source: " + deadEvent.getSource());
   }
 }
