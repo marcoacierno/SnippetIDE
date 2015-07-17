9e78c1e5c24cbee0fb392f7609efda22372c134b
Marco Acierno <marcoaciernoemail@gmail.com>
2015-07-18 01:56:49 +0200

    Remove assertConcurrent it's not used

--- Snippet-IDE/src/test/java/com/besaba/revonline/snippetide/configuration/JsonConfigurationTest.java
+++ Snippet-IDE/src/test/java/com/besaba/revonline/snippetide/configuration/JsonConfigurationTest.java
@@ -701,38 +701,4 @@ public class JsonConfigurationTest {
     assertEquals(98, newCopy.getAsInt("user.age").getAsInt());
     assertFalse(newCopy.getAsString("user.name").isPresent());
   }
-
-  public static void assertConcurrent(final String message, final List<? extends Runnable> runnables, final int maxTimeoutSeconds) throws InterruptedException {
-    final int numThreads = runnables.size();
-    final List<Throwable> exceptions = Collections.synchronizedList(new ArrayList<Throwable>());
-    final ExecutorService threadPool = Executors.newFixedThreadPool(numThreads);
-    try {
-      final CountDownLatch allExecutorThreadsReady = new CountDownLatch(numThreads);
-      final CountDownLatch afterInitBlocker = new CountDownLatch(1);
-      final CountDownLatch allDone = new CountDownLatch(numThreads);
-      for (final Runnable submittedTestRunnable : runnables) {
-        threadPool.submit(new Runnable() {
-          public void run() {
-            allExecutorThreadsReady.countDown();
-            try {
-              afterInitBlocker.await();
-              submittedTestRunnable.run();
-            } catch (final Throwable e) {
-              exceptions.add(e);
-            } finally {
-              allDone.countDown();
-            }
-          }
-        });
-      }
-      // wait until all threads are ready
-      assertTrue("Timeout initializing threads! Perform long lasting initializations before passing runnables to assertConcurrent", allExecutorThreadsReady.await(runnables.size() * 10, TimeUnit.MILLISECONDS));
-      // start all test runners
-      afterInitBlocker.countDown();
-      assertTrue(message +" timeout! More than" + maxTimeoutSeconds + "seconds", allDone.await(maxTimeoutSeconds, TimeUnit.SECONDS));
-    } finally {
-      threadPool.shutdownNow();
-    }
-    assertTrue(message + "failed with exception(s)" + exceptions, exceptions.isEmpty());
-  }
 }
