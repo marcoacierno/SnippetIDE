5748288dd58d2365bf6b2b2c459115304d5ae0d2
Marco Acierno <marcoaciernoemail@gmail.com>
2015-07-17 19:40:15 +0200

    Add class which contains the constant strings of the settings

---
+++ Snippet-IDE/src/main/java/com/besaba/revonline/snippetide/configuration/contract/ConfigurationSettingsContract.java
@@ -0,0 +1,10 @@
+package com.besaba.revonline.snippetide.configuration.contract;
+
+public class ConfigurationSettingsContract {
+  public static class Keymap {
+    public static final String SECTION_NAME = "keymap";
+
+    public static final String COMPILE_KEY = SECTION_NAME + ".compile";
+    public static final String RUN_KEY = SECTION_NAME + ".run";
+  }
+}
