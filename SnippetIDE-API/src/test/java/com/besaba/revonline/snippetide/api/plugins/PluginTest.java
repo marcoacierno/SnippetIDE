cb0055c5b2f7a3f9d4622a68ddf38365aac3184f
Marco Acierno <marcoaciernoemail@gmail.com>
2015-07-23 23:29:23 +0200

    Add language load in the PluginManager

--- SnippetIDE-API/src/test/java/com/besaba/revonline/snippetide/api/plugins/PluginTest.java
+++ SnippetIDE-API/src/test/java/com/besaba/revonline/snippetide/api/plugins/PluginTest.java
@@ -1,5 +1,7 @@
 package com.besaba.revonline.snippetide.api.plugins;
 
+import com.besaba.revonline.snippetide.api.shareservices.ShareService;
+import com.google.common.collect.ImmutableList;
 import org.junit.Test;
 
 import java.util.Collections;
@@ -9,7 +11,7 @@ import static org.junit.Assert.*;
 public class PluginTest {
   @Test
   public void testSupportsVersionWithAVersionOfTheIdeNotSupported() throws Exception {
-    final Plugin plugin = new Plugin("Fake", "Fake", Version.parse("3.5"), Version.parse("0.5"), new String[0], Collections.emptyList());
+    final Plugin plugin = new Plugin("Fake", "Fake", Version.parse("3.5"), Version.parse("0.5"), new String[0], Collections.emptyList(), ImmutableList.<ShareService>of());
     final Version ideVersion = Version.parse("0.1");
 
     // IDE version 0.1 cannot run a plugin which requires at least 0.5
