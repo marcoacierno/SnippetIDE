4333e3dd4386332cc2c2c381de217fa107e672fe
Marco Acierno <marcoaciernoemail@gmail.com>
2015-07-24 14:47:09 +0200

    Add shutdown of the Executor service when the application unboot

--- Snippet-IDE/src/main/java/com/besaba/revonline/snippetide/events/manager/impl/EventBusEventManager.java
+++ Snippet-IDE/src/main/java/com/besaba/revonline/snippetide/events/manager/impl/EventBusEventManager.java
@@ -1,6 +1,7 @@
 package com.besaba.revonline.snippetide.events.manager.impl;
 
 import com.besaba.revonline.snippetide.api.events.Event;
+import com.besaba.revonline.snippetide.api.events.boot.UnBootEvent;
 import com.besaba.revonline.snippetide.api.events.manager.EventManager;
 import com.google.common.eventbus.DeadEvent;
 import com.google.common.eventbus.EventBus;
@@ -10,10 +11,20 @@ import org.jetbrains.annotations.NotNull;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
 
 public class EventBusEventManager implements EventManager {
   private final static Logger logger = Logger.getLogger(EventBusEventManager.class);
-  private final ExecutorService executorService = Executors.newSingleThreadExecutor();
+  private final ExecutorService executorService = Executors.newSingleThreadExecutor(new ThreadFactory() {
+    private final ThreadFactory originalThreadFactory = Executors.defaultThreadFactory();
+
+    @Override
+    public Thread newThread(final Runnable r) {
+      final Thread thread = originalThreadFactory.newThread(r);
+      thread.setName("EventManager-Thread");
+      return thread;
+    }
+  });
 
   private final EventBus eventBus = new EventBus((throwable, subscriberExceptionContext) -> {
     logger.fatal("Exception inside the EventBus");
@@ -49,5 +60,10 @@ public class EventBusEventManager implements EventManager {
     logger.fatal("Dead event!");
     logger.fatal("Event: " + deadEvent.getEvent());
     logger.fatal("Source: " + deadEvent.getSource());
+  }
+
+  @Subscribe
+  public void onUnboot(final UnBootEvent event) {
+    executorService.shutdown();
   }
 }
