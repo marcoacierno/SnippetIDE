f12aec34989e51b5c79cdc19b9d5d1ae26a943de
Marco Acierno <marcoaciernoemail@gmail.com>
2015-07-27 13:08:53 +0200

    Add auto-tab when enter is pressed

--- Snippet-IDE/src/main/java/com/besaba/revonline/snippetide/ui/CodeTextArea.java
+++ Snippet-IDE/src/main/java/com/besaba/revonline/snippetide/ui/CodeTextArea.java
@@ -2,8 +2,15 @@ package com.besaba.revonline.snippetide.ui;
 
 import javafx.beans.value.ObservableValue;
 import javafx.scene.control.TextArea;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyCodeCombination;
+import javafx.scene.input.KeyEvent;
+
+import java.util.stream.IntStream;
 
 public class CodeTextArea extends TextArea {
+  private final static KeyCodeCombination ENTER = new KeyCodeCombination(KeyCode.ENTER);
+
 
   public CodeTextArea() {
   }
@@ -14,10 +21,22 @@ public class CodeTextArea extends TextArea {
 
   {
     textProperty().addListener(this::onTextChanged);
+    addEventHandler(KeyEvent.KEY_PRESSED, this::onKeyPressed);
   }
 
   public void onTextChanged(final ObservableValue<? extends String> value,
                             final String oldValue,
                             final String newValue) {
+
   }
+
+  public void onKeyPressed(final KeyEvent event) {
+    if (ENTER.match(event)) {
+      // new line with same indentation level
+      insertText(getCaretPosition(), "\n\t");
+
+      event.consume();
+    }
+  }
+
 }
